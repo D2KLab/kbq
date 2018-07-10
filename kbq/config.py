b1e5733b849917639d562470292121cf44be3ed8
rifat963 <rifat.abir@gmail.com>
2018-07-10 17:17:44 +0200

    db uri

--- kbq/config.py
+++ kbq/config.py
@@ -5,9 +5,9 @@ from apscheduler.jobstores.mongodb import MongoDBJobStore
 class Config:
     MONGO_DBNAME = 'kbq'
     # mLab mongo link
-    #MONGO_URI = 'mongodb://rifat:abir963@ds119651.mlab.com:19651/kbq'
+    MONGO_URI = 'mongodb://rifat:abir963@ds119651.mlab.com:19651/kbq'
     # Local mongodb
-    MONGO_URI = 'mongodb://localhost:27017/kbq'
+    #MONGO_URI = 'mongodb://localhost:27017/kbq'
     SECRET_KEY="powerful secretkey"
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
